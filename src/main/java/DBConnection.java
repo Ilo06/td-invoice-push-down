26edcc2e1d37f9310cd9b3f57965cbb9126efc5b
Ilo <hei.iloniaina@gmail.com>
2026-02-18 15:33:28 +0300

    feat: implements dbconnection for initialization

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    public Connection getConnection() {
+        try {
+            String jdbcURl = "jdbc:postgresql://localhost:5432/invoice"; //
+            String user = "postgres";
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/invoice", "postgres", "020606");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
