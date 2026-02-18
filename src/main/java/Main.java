81d362de0fe919b8e4ff301e735575458c31fe32
Ilo <hei.iloniaina@gmail.com>
2026-02-18 16:17:20 +0300

    feat: implements q2 findConfirmedAndInvoiceTotal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         DataRetriever dataRetriever = new DataRetriever();
 
-        System.out.println(dataRetriever.findInvoiceTotals());
+//        System.out.println(dataRetriever.findInvoiceTotals());
+        System.out.println(dataRetriever.findConfirmedAndPaidInvoiceTotals());
     }
 }
